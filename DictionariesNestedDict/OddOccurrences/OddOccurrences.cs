04ccfc1ddda2f6cf2be70223470182bc83b50cd3
Radostina Cvetkova <radost.tsvetkova@gmail.com>
2017-12-06 10:22:42 +0200

    Adding VS sln

---
+++ DictionariesNestedDict/OddOccurrences/OddOccurrences.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOccurrences
+{
+    class OddOccurrences
+    {
+        static void Main(string[] args)
+        {
+            var words = Console.ReadLine().Split();
+            var oddOccurrences = new Dictionary<string, int>();
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                string currentWord = words[i].ToLower();               
+
+                if (oddOccurrences.ContainsKey(currentWord))
+                {
+                    oddOccurrences[currentWord]++;
+                }
+                else
+                {
+                    oddOccurrences.Add(currentWord, 1);
+                }
+            }
+
+            var wordsWithOddOccurrences = new List<string>();
+
+            foreach (var word in oddOccurrences.Where(x => x.Value % 2 != 0))
+            {
+                wordsWithOddOccurrences.Add(word.Key);
+            }
+
+            Console.WriteLine(string.Join(", ", wordsWithOddOccurrences));
+        }
+    }
+}
